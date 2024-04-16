0441ff8aafae4aa423cd007e54b36165e501e72d
Angel Molina <jamg54.tech@gmail.com>
2024-04-15 19:15:09 -0600

    AVANCE EN POC DE CARGA BATCH

--- poc_postgress/src/main/java/com/postgres/poc/Main.java
+++ poc_postgress/src/main/java/com/postgres/poc/Main.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component
 public class Main implements CommandLineRunner {
@@ -13,6 +15,16 @@ public class Main implements CommandLineRunner {
     @Autowired
     PostgressCopy pc;
 
+    public void getDate(){
+        // Get the current date and time
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        // Define a custom format pattern
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        // Format the current date and time using the custom pattern
+        String formattedDateTime = currentDateTime.format(formatter);
+
+        System.out.println("\n\nCurrent Date: " + formattedDateTime);
+    }
     @Override
     public void run(String... args) throws Exception {
 
@@ -20,7 +32,8 @@ public class Main implements CommandLineRunner {
 
 
         System.out.println("\n INICIANDO....");
-        System.out.println("\n\nCurrent Date: " + LocalDate.now());
+
+        this.getDate();
 
         /*
         System.out.println("\n INSERTANDO PROMOCIONES....");
@@ -33,6 +46,7 @@ public class Main implements CommandLineRunner {
         );
         */
 
+        /*
         System.out.println("\n INSERTANDO SKUS....");
 
         pc.loadData(
@@ -40,17 +54,17 @@ public class Main implements CommandLineRunner {
                 "C:\\Users\\JAMG54\\Desktop\\LIVERPOOL\\POCS\\PostgresCOPY\\PROMOTIONS\\lp_institutional_promotion.txt.07032024",
                 new String[]{"sku","site_id","product_type","call_promo_service"}
         );
+        */
 
-        /*
         System.out.println("\n INSERTANDO RELACIONES....");
 
-        pc.loadData(
+        pc.loadPromotionRelations(
                 "sku_promo_relations",
                 "C:\\Users\\JAMG54\\Desktop\\LIVERPOOL\\POCS\\PostgresCOPY\\PROMOTIONS\\lp_instnl_sku_promotion.txt.07032024"
         );
 
-        System.out.println("\n\nCurrent Date: " + LocalDate.now());
-        */
+        this.getDate();
+
 
     }
 
--- poc_postgress/src/main/java/com/postgres/poc/components/PostgressCopy.java
+++ poc_postgress/src/main/java/com/postgres/poc/components/PostgressCopy.java
@@ -5,6 +5,9 @@ import org.postgresql.copy.CopyManager;
 import org.postgresql.core.BaseConnection;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import tech.tablesaw.api.StringColumn;
+import tech.tablesaw.api.Table;
+import tech.tablesaw.io.csv.CsvReadOptions;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,6 +16,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 @Component
 public class PostgressCopy {
 
@@ -25,32 +30,6 @@ public class PostgressCopy {
     @Value("${postgres.password}")
     private String password;
 
-
-    public void loadData(String tableName,String filePath) throws IOException {
-
-        try  {
-            System.out.println("URL:"+this.url);
-            System.out.println("USER:"+this.user);
-            System.out.println("PASSWORD:"+this.password);
-            System.out.println("PATH:"+filePath);
-
-            Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
-
-            CopyManager cp = new CopyManager((BaseConnection) conn);
-
-            String command="COPY "+tableName+" FROM STDIN WITH CSV DELIMITER '|'";
-
-            System.out.println("COMMAND:"+command);
-
-            long rowsInserted= cp.copyIn(
-                    command,
-                    new BufferedReader(new FileReader(filePath))
-                    );
-            System.out.printf("%d row(s) inserted%n", rowsInserted);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public void loadData(String tableName,String filePath, String [] fields){
 
@@ -78,6 +57,82 @@ public class PostgressCopy {
         }
 
     }
+
+
+    public void loadSKUS(String tableName,String filePath) throws IOException {
+
+        CsvReadOptions options = CsvReadOptions.builder(filePath)
+                .separator('|')
+                .header(false) // Indica si el archivo CSV tiene un encabezado
+                .build();
+
+
+
+        try  {
+
+            // Read the CSV file with the specified options
+            Table table = Table.read().csv(options);
+            StringColumn column1 = (StringColumn) table.column(1);
+
+            // Get the current date and time
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            // Define a custom format pattern
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            // Format the current date and time using the custom pattern
+            String formattedDateTime = currentDateTime.format(formatter);
+
+            //column1.set();
+
+            System.out.println("URL:"+this.url);
+            System.out.println("USER:"+this.user);
+            System.out.println("PASSWORD:"+this.password);
+            System.out.println("PATH:"+filePath);
+
+            Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
+
+            CopyManager cp = new CopyManager((BaseConnection) conn);
+
+            String command="COPY "+tableName+" FROM STDIN WITH CSV DELIMITER '|'";
+
+            System.out.println("COMMAND:"+command);
+
+            long rowsInserted= cp.copyIn(
+                    command,
+                    new BufferedReader(new FileReader(filePath))
+            );
+            System.out.printf("%d row(s) inserted%n", rowsInserted);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadPromotionRelations(String tableName,String filePath) throws IOException {
+
+        try  {
+            System.out.println("URL:"+this.url);
+            System.out.println("USER:"+this.user);
+            System.out.println("PASSWORD:"+this.password);
+            System.out.println("PATH:"+filePath);
+
+            Connection conn = DriverManager.getConnection(this.url, this.user, this.password);
+
+            CopyManager cp = new CopyManager((BaseConnection) conn);
+
+            String command="COPY "+tableName+" FROM STDIN WITH CSV DELIMITER '|'";
+
+            System.out.println("COMMAND:"+command);
+
+            long rowsInserted= cp.copyIn(
+                    command,
+                    new BufferedReader(new FileReader(filePath))
+                    );
+            System.out.printf("%d row(s) inserted%n", rowsInserted);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 
